876960a23c5cce9ceed8678786b66e91e7e908fc
SebastiaanWouters <sebastiaanwouters@tutanota.com>
2024-11-16 22:39:56 +0100

    fix: env variable usage

--- src/routes/api/download/+server.ts
+++ src/routes/api/download/+server.ts
@@ -1,10 +1,10 @@
 import { json } from '@sveltejs/kit';
 import { download } from '@sebas/open-book-api';
-import { API_KEY } from '$env/dynamic/private';
+import { env } from '$env/dynamic/private';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const { id, type } = await request.json();
-	const res = await download(id, type, API_KEY);
+	const res = await download(id, type, env.API_KEY);
 	return json(res);
 };
